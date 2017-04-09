a40b62a462ee7b0c47fc025878089d310980e7d7
dmazour <dmazour@gmail.com>
2017-04-09 15:53:36 -0500

    added AlarmView.swift

---
+++ Bonsai-FinalProject/Bonsai-FinalProject/AlarmView.swift
@@ -0,0 +1,31 @@
+//
+//  AlarmView.swift
+//  Bonsai-FinalProject
+//
+//  Created by Tarun Chally on 4/7/17.
+//  Copyright © 2017 wustl. All rights reserved.
+//
+
+import UIKit
+
+class AlarmView: UIViewController, UITextFieldDelegate{
+    
+    
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var date: UITextField!
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+        datePicker.minimumDate = Date()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
