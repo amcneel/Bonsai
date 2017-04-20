d3609130128b4cdbbec87f52ee921eca93f7297f
amcneel <Labuser@wtk316-33.cec.wustl.edu>
2017-04-20 00:29:27 -0500

    login-create-attempt1

--- Bonsai-FinalProject/Bonsai-FinalProject/SignInController.swift
+++ Bonsai-FinalProject/Bonsai-FinalProject/SignInController.swift
@@ -29,17 +29,74 @@ class SignInController: UIViewController{
     //////////////////////
     //at create Account
     @IBAction func createAccountBtn(_ sender: UIButton) {
-        FIRAuth.auth()?.createUser(withEmail: EmailCreate.text!, password: PasswordCreate.text!){
-            user, error in
-            if error == nil {
-                FIRAuth.auth()!.signIn(withEmail: self.UsernameSignin.text!, password: self.PasswordSignin.text!)
+        if EmailCreate.text == "" || PasswordCreate.text == ""{
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            FIRAuth.auth()?.createUser(withEmail: EmailCreate.text!, password: PasswordCreate.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully registered")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Account")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
             }
         }
     }
     
     //at login btn
     @IBAction func loginBtnPressed(_ sender: UIButton) {
-        FIRAuth.auth()?.signIn(withEmail: UsernameSignin.text!, password: PasswordSignin.text!)
+        if self.UsernameSignin.text == "" || self.PasswordSignin.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            FIRAuth.auth()?.signIn(withEmail: self.UsernameSignin.text!, password: self.PasswordSignin.text!) { (user, error) in
+                
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Account")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     
