97d4d4d54cabfe3a1d228e2c94cb2d796c0d67c2
amcneel <Labuser@wtk316-33.cec.wustl.edu>
2017-04-20 20:43:56 -0500

    okay tarun ill pull request damn

--- Bonsai-FinalProject/Bonsai-FinalProject/AppDelegate.swift
+++ Bonsai-FinalProject/Bonsai-FinalProject/AppDelegate.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FBSDKShareKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FBSDKApplicationDelegate.sharedInstance()
         FIRApp.configure()
         return true
     }
@@ -37,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
