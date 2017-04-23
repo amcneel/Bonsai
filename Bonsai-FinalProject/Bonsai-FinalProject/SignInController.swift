ee6a348afc6584a9d996cb970d58f2389151c038
amcneel <Labuser@wtk316-33.cec.wustl.edu>
2017-04-22 20:06:44 -0500

    fixed all sign in stuff
    title says all whaddup

--- Bonsai-FinalProject/Bonsai-FinalProject/SignInController.swift
+++ Bonsai-FinalProject/Bonsai-FinalProject/SignInController.swift
@@ -16,6 +16,7 @@ class SignInController: UIViewController, FBSDKLoginButtonDelegate{
 
     
     @IBOutlet var loginButton: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.delegate = self
